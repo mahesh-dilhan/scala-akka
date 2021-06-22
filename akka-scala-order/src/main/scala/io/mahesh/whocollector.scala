073c729462dbd18ee2ffe3dec5d0bbed24973ab0
mahesh <mail.mahesh.lk@gmail.com>
2021-06-22 18:27:49 +0800

    collector

--- akka-scala-order/src/main/scala/io/mahesh/whocollector.scala
+++ akka-scala-order/src/main/scala/io/mahesh/whocollector.scala
@@ -3,10 +3,20 @@ package io.mahesh
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.Behavior
+import io.mahesh.VaccineDistributor.Vaccine
 import io.mahesh.WhoCollector.Country
 
 
+object VaccineDistributor {
 
+  final case class Vaccine(country: String, volume: Long)
+
+  def apply() : Behavior[Vaccine] = Behaviors.receive {
+    (context,message) =>
+        context.log.info(s"${message}")
+      Behaviors.same
+  }
+}
 
 
 object WhoCollector {
@@ -14,10 +24,12 @@ object WhoCollector {
 
   def apply() : Behavior[Country] = Behaviors.setup {
     context =>
+     val vaccineDbtorRef = context.spawn(VaccineDistributor(),"vaccine")
       Behaviors.receiveMessage {
         message =>
           println(message.toString)
           context.log.info(message.toString)
+          vaccineDbtorRef ! Vaccine(message.country, message.postiveCases)
           Behaviors.same
       }
 
